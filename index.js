82baee88a529dde60839d37f68fd40a82baabf46
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-26 14:04:21 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const { stringifyRequest } = require("nock/lib/common");
+
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name:'Steve',
+      email: 'steve@steve.com'
+    };
+  
+    const configurationObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+  
+console.log(submitData())
